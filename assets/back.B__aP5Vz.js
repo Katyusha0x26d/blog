cc8e03a5317266f012f5c5136fa20a050f6189fa
Katyusha0x26d <Katyusha0x26d@users.noreply.github.com>
2025-05-23 11:43:52 +0000

    deploy: d57a9d45c749956f85020656bcc2186b1672ca7b

---
+++ assets/back.B__aP5Vz.js
@@ -0,0 +1,1 @@
+import{u as o}from"./chunks/vue-router.BZt0f7_Y.js";function t(){const e=o();function r(){document.referrer&&document.referrer!==location.href?e.back():e.push("/")}return{back:r}}export{t as u};
---
+++ assets/clone.BrxDIYFt.js
@@ -0,0 +1,1 @@
+import{b as r}from"./_baseUniq.BF4GBQ2i.js";var e=4;function a(o){return r(o,e)}export{a as c};
