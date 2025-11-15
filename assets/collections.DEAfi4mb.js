c4e29baba123fc14135c4350f3cac875abe208e2
Katyusha0x26d <Katyusha0x26d@users.noreply.github.com>
2025-11-15 14:53:01 +0000

    deploy: 02c896c698b550ac30905e0667cd88de08ded598

---
+++ assets/collections.DEAfi4mb.js
@@ -0,0 +1,1 @@
+import"./chunks/@vueuse/motion.CGoWS-TN.js";import{a as l}from"./chunks/vue-router.DLzm6UK4.js";import{A as t}from"./framework.BGZf6O3E.js";const e=[];function a(){return{collections:t(()=>e)}}function p(){const o=l(),n=t(()=>o.path.startsWith("/collections/")?o.path.split("/")[2]:"");return{collection:t(()=>e.find(c=>c.key===n.value))}}export{p as a,a as u};
---
+++ assets/index.B52zXkUL.js
@@ -0,0 +1,1 @@
+import"./chunks/@vueuse/motion.CGoWS-TN.js";import{n as s}from"./app.xq3tDqRN.js";import"./chunks/dayjs.DbKNreVD.js";import"./animation.Co_t4J6e.js";const m="/assets/none.DLM1AMsD.jpg";function p(t,o=m){const r=t.target;r.setAttribute("data-src",r.src),r.src=o}function c(t){return s(t,{template:"YYYY-MM-DD HH:mm:ss"})}export{c as f,p as o};
